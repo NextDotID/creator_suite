45a2f7724042c5ba4c745dc3d42f76f264262fe7
ZhongFuze <zella@mask.io>
2023-01-11 01:03:44 +0800

    demo: encrypt process

--- filebrowser/src/components/Ecies.vue
+++ filebrowser/src/components/Ecies.vue
@@ -10,89 +10,55 @@
     <v-window v-model="step">
       <v-window-item :value="1">
         <v-card-text>
-          <v-text-field prepend-icon="mdi-paperclip" label="Encrypt File" placeholder="Your File"
+          <v-text-field prepend-icon="mdi-paperclip" label="Your File" placeholder="Your File"
             v-model="origin_file"></v-text-field>
           <span class="text-caption text-grey-darken-1">
-
+            <br />
           </span>
-          <v-text-field prepend-icon="mdi-shield-key-outline" label="Password" type="password" v-model="key"
-            hint="Please enter a password"></v-text-field>
           <v-file-input :rules="rules" accept="image/png, image/jpeg, image/bmp" placeholder="Pick an avatar"
             prepend-icon="mdi-camera" label="Avatar" v-model="selectedFiles">
 
           </v-file-input>
           <v-textarea color="#cb87f4" label="Decription" v-model="description"
             hint="Please add some words to share and promote"></v-textarea>
-
           <p :style="classObj">{{ message }}</p>
         </v-card-text>
       </v-window-item>
 
       <v-window-item :value="2">
         <v-card-text>
-          <!-- <v-text-field label="Password" type="password"></v-text-field>
-          <v-text-field label="Confirm Password" type="password"></v-text-field> -->
-          <v-text-field label="Host" v-model="store.host"></v-text-field>
-          <v-text-field label="API Port" v-model="store.api_port"></v-text-field>
-          <v-text-field label="GateWay Port" v-model="store.gateway_port"></v-text-field>
-          <v-text-field label="PeerID" type="password" v-model="store.peer_id"></v-text-field>
-          <v-text-field label="Public Key" type="password" v-model="store.pub_key"></v-text-field>
-          <span class="text-caption text-grey-darken-1">
-            Please enter IPFS Connection Params
-          </span>
-          <p :style="classObj">{{ message }}</p>
-        </v-card-text>
-      </v-window-item>
-
-      <v-window-item :value="3">
-        <div class="pa-4 text-center">
-          <!-- <h3 class="text-h6 font-weight-light mb-2">Welcome to Vuetify</h3>
-          <span class="text-caption text-grey">Thanks for signing up!</span> -->
           <v-text-field label="Location Url" v-model="content_url"></v-text-field>
-          <v-text-field label="Key ID" v-model="key_id"></v-text-field>
           <v-select :items="['ethereum', 'goerli', 'polygon', 'mumbai']" label="Network" v-model="network"></v-select>
           <v-text-field label="Managed Contract" v-model="managed_contract"></v-text-field>
           <v-text-field label="Payment Token Address" v-model="payment_token_address"></v-text-field>
           <v-text-field label="Payment Token Amount" v-model="payment_token_amount"></v-text-field>
           <p :style="classObj">{{ message }}</p>
-        </div>
+        </v-card-text>
       </v-window-item>
     </v-window>
-
     <v-divider></v-divider>
-
     <v-card-actions>
       <v-btn v-if="step > 1" variant="text" @click="back"> Back </v-btn>
       <v-spacer></v-spacer>
-      <v-btn v-if="step == 1" class="vbutton" @click="create"> Encrypt </v-btn>
-      <v-btn v-if="step == 2" class="vbutton" @click="upload"> Upload </v-btn>
-      <v-btn v-if="step == 3" class="vbutton" @click="create_content">
-        Create
-      </v-btn>
-      <!-- <v-btn v-if="step < 3" color="primary" variant="flat" @click="step++">
-        Next
-      </v-btn> -->
-      <v-btn v-if="step < 3" @click="next">
+      <v-btn v-if="step == 1" class="vbutton" @click="save"> Save </v-btn>
+      <v-btn v-if="step == 2" class="vbutton" @click="create_content"> Create </v-btn>
+      <v-btn v-if="step < 2" @click="next">
         Next
         <v-icon>mdi-chevron-right</v-icon>
       </v-btn>
     </v-card-actions>
   </v-card>
+
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { useIpfsStore, useBrowserStore } from '@/stores'
+import { useBrowserStore } from '@/stores'
 import http from '@/plugins/http-common'
-import { IpfsCfg, IpfsUpload, IpfsUploadResult } from '@/types/ipfs'
-import { CreateRequest, CreateResponse, MoveRequest } from '@/types/file'
+import { CopyRequest } from '@/types/file'
 import { CreateContentRequest, CreateContentResponse } from '@/types/content'
 import ResponseData from '@/types/ResponseData'
-// const peer_id = storeToRefs(store)
-// const pub_key = storeToRefs(store)
-// const host = storeToRefs(store)
-// const api_port = storeToRefs(store)
-// const gateway_port = storeToRefs(store)
+
 export default defineComponent({
   props: {
     type: Number,
@@ -101,8 +67,6 @@ export default defineComponent({
     origin_file: String,
   },
   setup(props) {
-    var encrypt_file = props.origin_file + '.enc'
-    var encrypt_name = props.name + '.enc'
     var extension = props.extension
     var origin_file = props.origin_file
 
@@ -110,23 +74,17 @@ export default defineComponent({
       return !value || !value.length || value[0].size < 2000000 || 'Avatar size should be less than 2 MB!'
     }
 
-    const store = useIpfsStore()
     const browser = useBrowserStore()
     return {
-      store,
       browser,
       rules,
       origin_file,
-      encrypt_name,
-      encrypt_file,
       extension,
     }
   },
   data: () => ({
     create_msg: '',
     step: 1,
-    key: '',
-    key_id: -1,
     message: '',
     message_color: 1,
     content_url: '',
@@ -154,10 +112,8 @@ export default defineComponent({
     currentTitle() {
       switch (this.step) {
         case 1:
-          return 'Encrypt file by password'
+          return 'Choose file for Ecies encryption'
         case 2:
-          return 'Upload encrypted file to IPFS'
-        case 3:
           return 'Create Content'
         default:
           return 'CreatorSuite'
@@ -173,46 +129,20 @@ export default defineComponent({
       this.step++
       this.message = ''
     },
-    async test_img() {
-      var a: File = this.selectedFiles[0]
-      var arr = await a.arrayBuffer()
-      var view = new DataView(arr)
-      var c = new Blob([view.buffer]);
-    },
-    create() {
-      var req: CreateRequest = {
-        encrypt_type: Number(this.type),
-        key: this.key,
-        origin_file: this.origin_file!,
-        encrypt_file: this.encrypt_file,
-      }
-      console.log(req)
-      http
-        .post('/api/v1/file/create', req)
-        .then((response: CreateResponse) => {
-          console.log(response.data)
-          console.log(response.data.encrypt_file)
-          this.encrypt_file = response.data.encrypt_file
-          this.key_id = response.data.key_id
-          this.message = 'Encrypt content finished! key id is ' + this.key_id
-          this.message_color = 1
-        })
-        .catch((e: any) => {
-          console.log(e)
-          this.message = `Encrypt Failed ' + "${e.response.data.message} "`
-          this.message_color = 2
-        })
+    save() {
+      this.message = 'Save Content Succeeded'
+      this.message_color = 1
     },
     create_content() {
       var req: CreateContentRequest = {
-        content_locate_url: this.content_url,
+        content_locate_url: '',
         content_name: this.name!,
         managed_contract: this.managed_contract,
         network: this.network,
         payment_token_address: this.payment_token_address,
         payment_token_amount: Number(this.payment_token_amount),
-        key_id: Number(this.key_id),
-        encryption_type: Number(this.type),
+        key_id: -1,
+        encryption_type: Number(this.type), // ecies
         file_extension: this.extension!,
         description: this.description
       }
@@ -230,14 +160,14 @@ export default defineComponent({
             '/' +
             this.content_id.toString() +
             '/' +
-            this.encrypt_name
+            this.name
           console.log('target: ', target)
-          var move_req: MoveRequest = {
-            src: this.encrypt_file,
+          var move_req: CopyRequest = {
+            src: this.origin_file!,
             dst: target,
           }
           http
-            .post('api/v1/file/move', move_req)
+            .post('api/v1/file/copy', move_req)
             .then((response: ResponseData) => {
               console.log(response.data)
             })
@@ -251,30 +181,7 @@ export default defineComponent({
           this.message_color = 2
         })
     },
-    upload() {
-      var cfg: IpfsCfg = this.store.getIpfsCfg()
-      var req: IpfsUpload = {
-        local_file: this.encrypt_file,
-        cfg: cfg,
-      }
-      console.log(req)
-      http
-        .post('/api/v1/ipfs/upload', req)
-        .then((response: IpfsUploadResult) => {
-          console.log(response.data)
-          console.log(response.data.content_locate_url)
-          this.content_url = response.data.content_locate_url
-          this.message = 'Upload Succeeded: ' + response.data.content_locate_url
-          this.message_color = 1
-        })
-        .catch((e: any) => {
-          console.log(e)
-          this.content_url = ''
-          this.message = `Upload Failed: ' + "${e.response.data.message} "`
-          this.message_color = 2
-        })
-    },
-  },
+  }
 })
 </script>
 
